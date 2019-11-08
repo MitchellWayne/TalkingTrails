78e32008e46320bb211b169fe327e4848d74c504
MitchellWayne <MWayneMercado@gmail.com>
2019-11-07 20:18:30 -0800

    Added Alex's image pulling code
    Merged with mapview

--- app/src/main/java/com/example/untitledcs118proj/UploadActivity.java
+++ app/src/main/java/com/example/untitledcs118proj/UploadActivity.java
@@ -1,16 +1,78 @@
 package com.example.untitledcs118proj;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UploadActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload;
+    Button uploadButton;
+    EditText caption;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_upload);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        uploadButton = (Button) findViewById(R.id.uploadImage);
+        caption = (EditText) findViewById(R.id.caption);
+
+        imageToUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+        }
+    }
+
+    private class UploadImage extends AsyncTask<Void, Void, Void> {
+        Bitmap image;
+        String name;
+
+        public UploadImage(Bitmap image, String name) {
+            this.image = image;
+            this.name = name;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
     }
 
     public void returnMap(View view) {
